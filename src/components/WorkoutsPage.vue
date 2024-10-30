a7760c6f90aa435f1e17f4f2e5c3f846318b6442
Zack <168221270+Zval321@users.noreply.github.com>
2024-10-30 18:27:05 -0500

    index action

--- src/components/WorkoutsPage.vue
+++ src/components/WorkoutsPage.vue
@@ -1,9 +1,36 @@
 <script>
+import axios from 'axios'
+
+export default { 
+  props: { 
+    Workouts: Array
+  },
+  data: function () { 
+    return { 
+      workouts: [],
+    }
+  },
+  created: function () { 
+    this.handleIndexWorkouts();
+  },
+  methods: { 
+    handleIndexWorkouts: function () { 
+      axios.get('http://localhost:3000/workouts.json').then((response) => { 
+        console.log('handle index', response);
+        this.workouts = response.data
+      })
+    }
+  }
+}
 
 </script>
-
 <template>
-  <h1>Howdy</h1>
+  <div v-for="workout in workouts" v-bind:key="workout.id">
+    <h2>{{ workout.name }}</h2>
+    <h2> {{ workout.musclegroup }}</h2>
+    <h2> {{ workout.duration }}</h2>
+  </div>
+  
 </template>
 
 <style></style>
